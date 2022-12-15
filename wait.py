6898d8fc21da7da0d185c9c3322d56892e217e22
Marripelli Madhu <Marripelli.Madhu@blueyonder.com>
2022-12-15 22:18:36 +0530

    tactical

--- wait.py
+++ wait.py
@@ -16,7 +16,7 @@ logging.basicConfig(
     ]
 )
 
-gh_access_token = 'token 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'
+gh_access_token = 'token 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'
 
 headers = {
     'Authorization': gh_access_token
